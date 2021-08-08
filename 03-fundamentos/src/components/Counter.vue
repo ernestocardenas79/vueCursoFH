765602e0c260038de53738863577fd4ff993c86c
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2021-08-08 08:34:53 -0600

    Inciando app Counter de react

---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,56 @@
+<template>
+  <h2>{{ customTitle }} {{ start }}</h2>
+  <p>
+    {{ counter }} <sup>{{ squareCounterValue }}</sup>
+  </p>
+  <div>
+    <button @click="increase">+1</button><button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 100,
+      validator(value) {
+        return value > -1;
+      },
+    },
+  },
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  computed: {
+    squareCounterValue() {
+      return this.counter * this.counter;
+    },
+    customTitle() {
+      return this.title || "Counter";
+    },
+  },
+  methods: {
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      this.counter--;
+    },
+  },
+};
+</script>
+<style>
+button {
+  border-color: #64bb87;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px;
+}
+</style>
