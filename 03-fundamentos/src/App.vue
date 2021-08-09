fd9510109b316308f9df03193037a1dcf0d5cd45
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2021-08-09 11:10:38 -0600

    Se crea componente indesicion: 	* Se conecta con la api yesno.wtf 	* el usuario captura la pregunta para que sea respondida 	* tiene funcionalidad para mostrar la pregunta hecha por el usuario junto con su pregunta

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Counter :start="101" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <Indecision />
 </template>
 
 <script>
 import Counter from "@/components/Counter.vue";
+import Indecision from '@/components/Indecision.vue';
 
 export default {
   name: "App",
   components: {
-    Counter,
+    //Counter,
+    Indecision
   },
 };
 </script>
