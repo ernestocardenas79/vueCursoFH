dbaed98c58db1b21026814a8bf240a3f8bd2184a
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2021-08-24 07:57:39 -0600

    identificar si la path del router cambia lanzar el componente viewEdit

--- 07-journal/src/router/index.js
+++ 07-journal/src/router/index.js
@@ -6,6 +6,12 @@ const routes = [
     path: "/",
     component: () => import(/*webpackChunkName Home*/ "@/views/Home.vue"),
   },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/*webpackChunkName About*/ import("../views/About")),
+  },
   ...daybookRoutes,
 ];
 
